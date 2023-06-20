4b18438ab9b1077dc8fef47e12a64c7629726fbc
Mwanzia24 <mwanzaitom19@gmail.com>
2023-06-20 06:13:39 -0400

    done

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,4 +1,5 @@
 class PlantsController < ApplicationController
+  before_action :set_plant, only: [:show, :update, :destroy]
 
   # GET /plants
   def index
@@ -8,8 +9,7 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
-    render json: plant
+    render json: @plant
   end
 
   # POST /plants
@@ -18,7 +18,26 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  # PATCH /plants/:id
+  def update
+    if @plant.update(plant_params)
+      render json: @plant
+    else
+      render json: @plant.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /plants/:id
+  def destroy
+    @plant.destroy
+    head :no_content
+  end
+
   private
+
+  def set_plant
+    @plant = Plant.find(params[:id])
+  end
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
